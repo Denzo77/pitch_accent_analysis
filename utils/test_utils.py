b5552e06d2575cc1073a7a5959a872cabfcee8c3
Deniz Erbilgin <deniz.erbilgin@vestemi.com>
2020-02-10 18:05:07 +0000

    do tests properly

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -8,6 +8,8 @@ class TestKanaPhonemeExtraction():
     unigraphs = 'ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンッ'
 
     # Use linear combinations to generate all possible digraphs.
+    # Not all combinations are strictly valid, but this should form a superset
+    # of digraphs we're interested in.
     digraphs = [
         "".join((x, y)) 
         for x in 'ギジヂビピキシチニヒミリ'  # diagraph_beginings
@@ -67,12 +69,115 @@ class TestKanaPhonemeExtraction():
             'mora': [[utils.Mora(x, 0)] for x in self.unigraphs],
         })
 
-        utils.get_last_mora_info(df)
+        expected = pd.DataFrame({
+            'onset': [
+                'g', 'g', 'g', 'g', 'g',
+                'z', 'z', 'z', 'z', 'z',
+                'd', 'd', 'd', 'd', 'd',
+                'b', 'b', 'b', 'b', 'b',
+                'p', 'p', 'p', 'p', 'p',
+                'a', 'i', 'u', 'e', 'o',
+                'k', 'k', 'k', 'k', 'k',
+                's', 's', 's', 's', 's',
+                't', 't', 't', 't', 't',
+                'n', 'n', 'n', 'n', 'n',
+                'h', 'h', 'h', 'h', 'h',
+                'm', 'm', 'm', 'm', 'm',
+                'y', 'y', 'y', 'r', 'r',
+                'r', 'r', 'r', 'w', 'w',
+                False, False
+            ],
+            'is_vowel': [
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                True, True, True, True, True,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False, False, False, False,
+                False, False
+            ],
+            'end_vowel': [
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'i', 'u', 'e', 'o',
+                'a', 'u', 'o', 'a', 'i',
+                'u', 'e', 'o', 'a', 'o',
+                False, False
+            ],
+        })
+
+        result = utils.get_last_mora_info(df).drop('mora', axis=1)
+        
+        pd.testing.assert_frame_equal(result, expected)
 
 
     def test_pass_if_get_last_mora_info_works_with_all_digraphs(self):
         df = pd.DataFrame({
             'mora': [[utils.Mora(x, 0)] for x in self.digraphs],
         })
-        
-        utils.get_last_mora_info(df)
+
+        expected = pd.DataFrame({
+            'onset': [
+                'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 
+                'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z', 
+                'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 
+                'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 
+                'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 
+                'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 
+                's', 's', 's', 's', 's', 's', 's', 's', 
+                't', 't', 't', 't', 't', 't', 't', 't', 
+                'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 
+                'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 
+                'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 
+                'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'
+            ],
+            'is_vowel': [
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False,
+                False, False, False, False, False, False, False, False
+            ],
+            'end_vowel': [
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o',
+                'a', 'i', 'u', 'e', 'o', 'a', 'u', 'o'
+            ],
+        })
+
+        result = utils.get_last_mora_info(df).drop('mora', axis=1)
+
+        pd.testing.assert_frame_equal(result, expected)
